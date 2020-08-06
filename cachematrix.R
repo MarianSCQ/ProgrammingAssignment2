1aa095c3b1a91d495f127945154bd1bf04d6c33a
MarianSCQ <marian234b@gmail.com>
2020-08-06 15:43:48 -0400

    Functions for Programming Assigment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function makes a special matriz object that can cache its inverse
+## assumptions are that matrix are always invertible (same number of columns 
+## and rows)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        s <- NULL
+        set <- function(y = matrix()) {
+                x <<- y*
+                s <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) s <<- solve
+        getsolve<- function() s 
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+## This function computes the inverse of the special matriz given by 
+## makeCacheMatrix above. 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        s <- x$getsolve()
+        if(!is.null(s)) {
+                message("getting cached data!")
+                return(s)
+        }
+        data <- x$get()
+        inverse <- solve(data, ...)
+        x$setinverse(inverse)
+        return(inverse)
 }
+
+## Error in x$getinverse : $ operator is invalid for atomic vectors
+
+
+
+
+
+
+
+
+
+
+
